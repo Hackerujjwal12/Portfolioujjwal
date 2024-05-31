ee20adb9200be1846ebc4cd9ecd43fcec32949fc
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-05-31 10:42:24 +0545

    refactor: Homepage

--- src/Portfolio/Homepage.jsx
+++ src/Portfolio/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './Navbar';
 
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 export default function Homepage() {
   return (
-    <div className='flex w-screen h-screen flex-col mx-auto'>
+    <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/Portfolio/Navbar.jsx
+++ src/Portfolio/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className='flex container mx-auto items-center justify-between border-b-2 p-3'>
       <div className='text-xl font-bold '>BB YT .</div>
-      <div className='flex'>
+      <div className=' hidden md:flex'>
         {list.map((item) => (
           <div className='m-1 p-2' key={item.to}>
             <Link to={item.to}>{item.name}</Link>
